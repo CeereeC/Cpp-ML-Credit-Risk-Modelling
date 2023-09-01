d78c43b324a29e7d107075dfde3a6342a5c42bbe
CeereeC <myself3216@gmail.com>
2023-09-01 15:49:28 +0800

    Add data setup

--- main.cpp
+++ main.cpp
@@ -12,6 +12,18 @@ int main() {
   data::DatasetInfo info;
   data::Load("data/cleaned_credit_data.csv", dataset, info); // Remember that Load(...) transposes the matrix
 
+
+  // ============ Preprocess Data ============= //
+
+  arma::mat trainData, testData;
+  data::Split(dataset, trainData, testData, RATIO);
+
+  arma::mat trainX = trainData.submat(0, 0, trainData.n_rows - 2, trainData.n_cols - 1);
+  arma::mat testX = testData.submat(0, 0, testData.n_rows - 2, testData.n_cols - 1);
+
+  arma::mat trainY = trainData.row(trainData.n_rows - 1);
+  arma::mat testY = testData.row(testData.n_rows - 1);
+
   return 0;
 }
 
